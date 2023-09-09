5915b7714481e8700c67e4921baa48a7e0419d5a
RupanshuTandon <rupanshu_tandon@cargill.com>
2023-09-09 12:29:51 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      // You can extend the default theme and customize it here.
+      // For example, you can add additional colors, fonts, or spacing options.
+    },
+  },
+  variants: {
+    extend: {
+      // You can extend or customize the default variants for utilities here.
+      // For example, you can control the hover, focus, or active states of elements.
+    },
+  },
+  plugins: [
+    // You can include additional plugins here, such as custom components or utilities.
+  ],
+}
